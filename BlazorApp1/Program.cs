acc60a11b25bddb25f2ac9ac869f4e2f86a30c74
Tony82122 <143779031+Tony82122@users.noreply.github.com>
2024-11-01 23:16:09 +0100

    yes

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddHttpClient();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5062") });
 builder.Services.AddScoped<IUserService, HttpUserService>();
 builder.Services.AddScoped<IPostService, HttpPostService>();
